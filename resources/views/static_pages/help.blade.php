f1b71338b1860164b296b69199e15cf4e763c4f0
neekko33 <neekko33@outlook.com>
2024-08-09 16:21:43 +0800

    add template;

--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Weibo App</title>
-</head>
-<body>
-<h1>帮助页</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('title', '帮助')
+
+@section('content')
+  <h1>帮助页</h1>
+@stop
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,13 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Weibo App</title>
-</head>
-<body>
-<h1>主页</h1>
-</body>
-</html>
+@extends('layouts.default')
+
+@section('content')
+  <h1>主页</h1>
+@stop

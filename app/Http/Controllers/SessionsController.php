726acc9c1e74b901da1e06286e59ac21418ec39f
neekko33 <neekko33@outlook.com>
2024-08-11 19:22:58 +0800

    add sessions;

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController
+{
+    public function create(): View|Factory|Application
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', 'You are now logged in');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', 'email or password is incorrect');
+            return redirect()->back()->withInput();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\SessionsController;
 
 Route::get('/', [StaticPagesController::class, 'home'])->name('home');
 Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
@@ -10,3 +11,7 @@ Route::get('/about', [StaticPagesController::class, 'about'])->name('about');
 
 Route::get('signup', [UsersController::class, 'create'])->name('signup');
 Route::resource('users', UsersController::class);
+
+Route::get('login', [SessionsController::class, 'create'])->name('login');
+Route::post('login', [SessionsController::class, 'store'])->name('login');
+Route::delete('logout', [SessionsController::class, 'destroy'])->name('logout');

dff9e639d654260e0a957466f39e2160b7d9613c
neekko33 <neekko33@outlook.com>
2024-08-13 21:54:06 +0800

    update routes;

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,27 @@ use App\Http\Controllers\StaticPagesController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\SessionsController;
 
-Route::get('/', [StaticPagesController::class, 'home'])->name('home');
-Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
-Route::get('/about', [StaticPagesController::class, 'about'])->name('about');
+Route::controller(StaticPagesController::class)->group(function () {
+    Route::get('/', 'home')->name('home');
+    Route::get('/help', 'help')->name('help');
+    Route::get('/about', 'about')->name('about');
+});
 
-Route::get('signup', [UsersController::class, 'create'])->name('signup');
 Route::resource('users', UsersController::class);
 
-Route::get('login', [SessionsController::class, 'create'])->name('login');
-Route::post('login', [SessionsController::class, 'store'])->name('login');
-Route::delete('logout', [SessionsController::class, 'destroy'])->name('logout');
+Route::controller(UsersController::class)->group(function () {
+    Route::get('signup', 'create')->name('signup');
+    Route::get('/users/{user}/followings', 'followings')->name('users.followings');
+    Route::get('/users/{user}/followers', 'followers')->name('users.followers');
+});
+
+Route::controller(SessionsController::class)->group(function () {
+    Route::get('login', 'create')->name('login');
+    Route::post('login', 'store')->name('login');
+    Route::delete('logout', 'destroy')->name('logout');
+});
 
 Route::resource('statuses', StatusesController::class, ['only' => ['store', 'destroy']]);
-
-Route::get('/users/{user}/followings', [UsersController::class, 'followings'])->name('users.followings');
-Route::get('/users/{user}/followers', [UsersController::class, 'followers'])->name('users.followers');
 
 Route::post('/users/followers/{user}', [FollowersController::class, 'store'])->name('followers.store');
 Route::delete('users/followers/{user}', [FollowersController::class, 'destroy'])->name('followers.destroy');
